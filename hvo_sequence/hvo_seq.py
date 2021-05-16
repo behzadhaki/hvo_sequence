81c296d86dba7861178b4065a0511c9c173b8463
behzadhaki <behzad.haki@upf.edu>
2021-05-16 18:36:22 +0200

    V 0.4.1 fixed the NoneType bug for new_hvo_seq.remove_hvo()

--- hvo_sequence/hvo_seq.py
+++ hvo_sequence/hvo_seq.py
@@ -59,7 +59,7 @@ class HVO_Sequence(object):
         PATCH version when you make backwards compatible bug fixes.
         """
 
-        self.__version = "0.4.0"
+        self.__version = "0.4.1"
 
         self.__metadata = Metadata()
 
